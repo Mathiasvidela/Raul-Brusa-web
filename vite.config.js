b47e1762b94784c5efbe6d5a34212592befd4578
Mathias Videla <mathiasvidela20@gmail.com>
2024-07-29 18:15:38 +0200

    page 404 y routing

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig ({
             input:{
                 main: resolve(__dirname, 'index.html'),
                 gestionAmbiental: resolve(__dirname, 'src/pages/medioambiente/gestionAmbiental.html'),
+                informesAmbientales: resolve(__dirname, 'src/pages/medioambiente/informesAmbientales.html'),
+                monitoreosAmbientales: resolve(__dirname, 'src/pages/medioambiente/monitoreosAmbientales.html'),
+                capacitacionesAmbientales: resolve(__dirname, 'src/pages/medioambiente/capacitacionesAmbientales.html'),
                 aboutUs: resolve(__dirname, 'src/pages/aboutus.html')
             }
         }
